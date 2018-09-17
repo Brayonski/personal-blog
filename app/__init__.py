d294cf7e382fb634477d1f2a954ab6cbcd50216e
root <root@localhost.localdomain>
2018-09-17 11:47:00 +0300

    simplemde connection to app template

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 from config import config_options
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -15,6 +16,8 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
+simple = SimpleMDE()
+
 
 # Initializing application
 def create_app(config_name):
@@ -23,6 +26,7 @@ def create_app(config_name):
 
         # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    simple.init_app(app)
 
 # configure UploadSet
     configure_uploads(app,photos)
--- config.py
+++ config.py
@@ -25,7 +25,12 @@ class TestConfig(Config):
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:mutaikips@localhost/blog'
-    DEBUG = True    
+    DEBUG = True  
+
+class Config:
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True  
 
 
 
