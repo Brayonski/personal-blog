daee5f72064f1dca0da0900cc71838a5649d36f7
root <root@localhost.localdomain>
2018-09-17 09:37:57 +0300

    deploying to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export SECRET_KEY='mutaikips'
 export MAIL_USERNAME='kiprotichbrian8@gmail.com'
 export MAIL_PASSWORD='mutaikips'
+export DATABASE_URL='postgres://xyvvuxczpwhzah:1a87f72cc8b15c99d3c3dac1280af55d9ffbe2da222eb3b63260759432280b92@ec2-174-129-32-37.compute-1.amazonaws.com:5432/df5hl0e4alv5c8'
 python3.6 manage.py server
